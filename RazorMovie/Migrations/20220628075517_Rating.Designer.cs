f12f59824d2094974ccf835f21af3e03327604a7
ammwangi <elijah.mwangi@kamsware.com>
2022-06-28 11:13:04 +0300

    updates and migrations

--- RazorMovie/Migrations/20220628075517_Rating.Designer.cs
+++ RazorMovie/Migrations/20220628075517_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorMovie.Data;
 
 namespace RazorMovie.Migrations
 {
     [DbContext(typeof(RazorMovieContext))]
-    partial class RazorMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20220628075517_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
